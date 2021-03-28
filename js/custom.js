5c464c6b49139663bfa1a88fe79b098182af8c41
Marwan Elsayed <marwanellsayed@hotmail.com>
2021-03-28 19:55:24 +0200

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+$(function () {
+
+  'use strict';
+
+// make header full screen
+
+  $(".header").height($(window).height());
+  $(window).resize(function () {
+    $(".header").height($(window).height());
+  });
+
+//  fixed navbar for mobile
+
+$(".navbar-toggler").click(function () {
+  $(".collapse").slideToggle();
+});
+
+// creat up button
+
+$(".up").click(function () {
+  $("html, body").animate({
+    scrollTop: 0
+  });
+});
+
+// hide up button
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= $(".header").height()) {
+    $(".up").fadeIn(500);
+  } else {
+    $(".up").hide();
+  }
+
+});
+
+});
